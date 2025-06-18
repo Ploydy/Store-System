b865e2f1f462fe49d26f8dce2c5d8c9a2beece88
Ploydy <Lfecarballo20@gmail.com>
2025-06-18 15:30:40 +0800

    dashboard cards added

--- store/src/app/dashboard/(overview)/page.tsx
+++ store/src/app/dashboard/(overview)/page.tsx
@@ -1,14 +1,16 @@
-/* import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+/* 
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+*/
 import CardWrapper from '@/app/ui/dashboard/cards';
- */
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
-/* import {
-  RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton,
-} from '@/app/ui/skeletons';
-*/
-import { Metadata } from 'next'; 
+import { Metadata } from 'next';
+import {
+  CardsSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton
+} from '@/app/ui/dashboard/skeletons';
 
 
 export const metadata: Metadata = {
@@ -24,19 +26,19 @@ export default async function Page() {
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
+          {/* <RevenueChart /> */}
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
+          {/* <LatestInvoices /> */}
         </Suspense>
-      </div> */}
+      </div>
     </main>
   );
 }
